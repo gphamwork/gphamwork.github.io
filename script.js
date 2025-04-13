8d397e4ba7a0651dac693a98f1c0f0e4f06ee64c
Giang Pham <gpham@mac.mynetworksettings.com>
2025-04-13 08:38:59 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const enterButton = document.getElementById('enterButton');
+const screen1 = document.getElementById('screen1');
+const screen2 = document.getElementById('screen2');
+
+enterButton.addEventListener('click', () => {
+  anime({
+    targets: '#screen1',
+    opacity: [1, 0],
+    duration: 800,
+    easing: 'easeInOutQuad',
+    complete: () => {
+      screen1.classList.add('hidden');
+      screen2.classList.remove('hidden');
+
+      anime({
+        targets: '#screen2',
+        opacity: [0, 1],
+        duration: 800,
+        easing: 'easeInOutQuad'
+      });
+    }
+  });
+});
